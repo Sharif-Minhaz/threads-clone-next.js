4ece691fc1987138ecdb5a851c84fc16b59c745e
Minhaz <sharif35-3001@diu.edu.bd>
2023-10-25 14:03:55 +0600

    feat: all option bars added

---
+++ components/shared/RightSideBar.tsx
@@ -0,0 +1,12 @@
+export default function RightSideBar() {
+  	return (
+		<section className="rightsidebar custom-scrollbar">
+			<div className="flex flex-1 justify-start flex-col">
+				<h3 className="text-light-1 text-heading4-medium">Suggested Communities</h3>
+			</div>
+			<div className="flex flex-1 justify-start flex-col">
+				<h3 className="text-light-1 text-heading4-medium">Suggested Users</h3>
+			</div>
+		</section>
+  	)
+}
